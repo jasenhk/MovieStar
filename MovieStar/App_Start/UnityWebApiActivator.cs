f801dfcd2bb71cbdfafc956e3589b596685770b8
Jasen Katagihara <jasen.katagihara@gmail.com>
2018-08-08 08:03:39 -1000

    Update packages

--- MovieStar/App_Start/UnityWebApiActivator.cs
+++ MovieStar/App_Start/UnityWebApiActivator.cs
@@ -15,7 +15,7 @@ namespace MovieStar
         /// <summary>
         /// Integrates Unity when the application starts.
         /// </summary>
-        public static void Start()
+        public static void Start() 
         {
             // Use UnityHierarchicalDependencyResolver if you want to use
             // a new child container for each IHttpController resolution.
